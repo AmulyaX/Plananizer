f05dbb85af6f32a932449df9ad82f6be3021fd36
AmulyaX <amulya.b520@gmail.com>
2019-08-01 10:40:12 +0530

    spring-boot: Initialise project
    *Imported from https://start.spring.io/
    
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

---
+++ spring-boot/Plananizer/src/main/java/com/paytm/Plananizer/PlananizerApplication.java
@@ -0,0 +1,13 @@
+package com.paytm.Plananizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlananizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlananizerApplication.class, args);
+	}
+
+}
