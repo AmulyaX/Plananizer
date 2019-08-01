cbaeffe032bc6948192ce8b170a41cca5f577cb2
AmulyaX <amulya.b520@gmail.com>
2019-08-01 13:28:36 +0530

    reactjs: plananizer: src: Move icons to left in the sidebar
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- reactjs/plananizer/src/components/Sidebar/SidebarView.js
+++ reactjs/plananizer/src/components/Sidebar/SidebarView.js
@@ -102,10 +102,11 @@ const styles = theme => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 1,
   },
   sidebarList: {
     marginTop: theme.spacing.unit * 6,
+    marginLeft: -20
   },
   mobileBackButton: {
     marginTop: theme.spacing.unit * .5,
