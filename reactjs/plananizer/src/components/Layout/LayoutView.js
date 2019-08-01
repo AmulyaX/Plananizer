235e77e4e62270a5aefa44f5fa74ce5060df8a80
AmulyaX <amulya.b520@gmail.com>
2019-08-01 13:23:31 +0530

    reactjs: plananizer: src: Import Redirect
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- reactjs/plananizer/src/components/Layout/LayoutView.js
+++ reactjs/plananizer/src/components/Layout/LayoutView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withStyles, CssBaseline } from '@material-ui/core';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import classnames from 'classnames';
 
 import Header from '../Header';
